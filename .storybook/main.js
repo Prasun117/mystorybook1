ade4a8aae9cbc1eb56f056f7bd7a85891e7c79d7
Prasun117 <johnmukherjee1997@gmail.com>
2021-08-06 20:46:37 +0530

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.@(ts|tsx)"
+
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-knobs"
+
+  ],
+
+  
+}
